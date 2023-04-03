75e675897bfd173915ccfedf92d127181e58e630
61130061 <thezeroyou@gmail.com>
2023-04-03 10:24:03 +0700

    ♻️ update skill informations

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,9 +4,10 @@ function Skills () {
       <div className="text-4xl my-4 font-[400]">Skills</div>
       <div className="grid md:grid-cols-3 grid-cols-2 gap-5">
         <div>
-          <div className="font-[500] text-xl my-2">Programming</div>
+          <div className="font-[500] text-xl my-2">Language</div>
           <ul className="list-disc list-inside px-1">
             <li>Javascript</li>
+            <li>Typescript</li>
             <li>HTML</li>
             <li>CSS</li>
             <li>Python</li>
@@ -17,8 +18,9 @@ function Skills () {
         <div>
           <div className="font-[500] text-xl my-2">Frontend</div>
           <ul className="list-disc list-inside px-1">
-            <li>Node JS</li>
-            <li>React JS</li>
+            <li>NodeJS</li>
+            <li>ReactJS</li>
+            <li>ThreeJS</li>
             <li>React Native</li>
             <li>Tailwind CSS</li>
           </ul>
