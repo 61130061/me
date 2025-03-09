702f0abe07a0f6d23249404f178842baf9cfde2f
Warunyu (Yu) <warunyu@ttmi-th.com>
2025-03-09 20:37:53 +0700

    update to latest contents

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,16 +35,32 @@ function Header({ dark, setDark }) {
   return (
     <div>
       <div className="flex gap-3 flex-wrap flex-row-reverse text-sm">
-        <div onClick={() => setDark(!dark)} className="p-2 bg-black dark:bg-white hover:shadow-lg hover:cursor-pointer">
+        <div
+          onClick={() => setDark(!dark)}
+          className="p-2 bg-black dark:bg-white hover:shadow-lg hover:cursor-pointer"
+        >
           <Moon />
         </div>
-        <a href="https://github.com/61130061" target="_blank" className="p-2 bg-black dark:bg-white hover:shadow-lg hover:cursor-pointer">
+        <a
+          href="https://github.com/61130061"
+          target="_blank"
+          className="p-2 bg-black dark:bg-white hover:shadow-lg hover:cursor-pointer"
+        >
           <GitHub />
         </a>
-        <a href="https://linkedin.com/in/h-warunyu" target="_blank" className="py-2 px-3 bg-black dark:bg-white hover:shadow-lg hover:cursor-pointer">
+        <a
+          href="https://linkedin.com/in/h-warunyu"
+          target="_blank"
+          className="py-2 px-3 bg-black dark:bg-white hover:shadow-lg hover:cursor-pointer"
+        >
           <LinkedIn />
         </a>
-        <a href="mailto: h.warunyu@gmail.com" className="p-2 bg-black dark:bg-white dark:text-black text-white hover:shadow-lg hover:cursor-pointer">h.warunyu@gmail.com</a>
+        <a
+          href="mailto: h.warunyu@gmail.com"
+          className="p-2 bg-black dark:bg-white dark:text-black text-white hover:shadow-lg hover:cursor-pointer"
+        >
+          h.warunyu@gmail.com
+        </a>
       </div>
       <div className="text-5xl mb-5 mt-8 font-[600]">Warunyu Hematulin</div>
       <div>Make impossible things possible with code</div>
@@ -53,9 +69,15 @@ function Header({ dark, setDark }) {
         <div>{typing}</div>
         <div className="type-cursor">|</div>
       </div>
-      <div>Self-taught web developer and research engineer who likes making impossible thing for the better world with code. Now seeking to launch a career in web developer. Familiar with development and deployment process for many web developer frameworks specially in React and NextJS. Loves learning new technologies and solving problems by coding.</div>
+      <div>
+        Software engineer with rapid career growth, excelling in scalable
+        micro-services (gRPC, Kafka) and full-stack development (Go, Python,
+        React, Flutter). Potential for leading high-impact projects, delivering
+        optimized, cost- effective solutions. Passionate about AI, cloud, and
+        open-source innovation.
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,42 +1,47 @@
-function Skills () {
+function Skills() {
   return (
     <div className="my-12">
       <div className="text-4xl my-4 font-[400]">Skills</div>
-      <div className="grid md:grid-cols-3 grid-cols-2 gap-5">
+      <div className="grid md:grid-cols-4 grid-cols-2 gap-5">
         <div>
           <div className="font-[500] text-xl my-2">Language</div>
           <ul className="list-disc list-inside px-1">
             <li>Javascript</li>
             <li>Typescript</li>
-            <li>HTML</li>
-            <li>CSS</li>
             <li>Python</li>
-            <li>SQL</li>
-            <li>Swift</li>
+            <li>Go</li>
           </ul>
         </div>
         <div>
           <div className="font-[500] text-xl my-2">Frontend</div>
           <ul className="list-disc list-inside px-1">
-            <li>NodeJS</li>
-            <li>ReactJS</li>
-            <li>ThreeJS</li>
-            <li>React Native</li>
-            <li>Tailwind CSS</li>
+            <li>React</li>
+            <li>NextJS</li>
+            <li>Remix</li>
+            <li>Flutter</li>
           </ul>
         </div>
         <div>
           <div className="font-[500] text-xl my-2">Backend</div>
           <ul className="list-disc list-inside px-1">
-            <li>Rest API</li>
-            <li>Next JS</li>
-            <li>Prisma</li>
-            <li>Mongodb</li>
+            <li>ExpressJS</li>
+            <li>gRPC</li>
+            <li>Protobuf</li>
+          </ul>
+        </div>
+        <div>
+          <div className="font-[500] text-xl my-2">Other</div>
+          <ul className="list-disc list-inside px-1">
+            <li>Kafka</li>
+            <li>Odoo</li>
+            <li>AWS</li>
+            <li>Docker</li>
+            <li>Gitlab CI/CD</li>
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
